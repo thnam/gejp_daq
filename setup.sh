2eddb76d8f4e2ac54624eff9532075a27e6d141f
nam <thnam.me@gmail.com>
2015-03-18 11:52:00 +0900

    improve setup script: check for exptab before create one

--- setup.sh
+++ setup.sh
@@ -1,20 +1,16 @@
 #!echo You must source
 
-#export CVS_RSH=ssh
-#export MIDASSYS=/home/nam/sw/midas/
-#export PATH=.:$MIDASSYS/linux/bin:/home/nam/sw/roody/bin:$PATH
+if [[ ! -f exptab ]]; then
+  echo "No exptab found, will generate one ..."
+  expname=${PWD##*/}
+  rndstr=$(tr -dc "[:alpha:]" < /dev/urandom | head -c 8)
 
-# Get experiment name from the current directory
-# add a random string to make sure no duplication in client name
-expname=${PWD##*/}
-rndstr=$(tr -dc "[:alpha:]" < /dev/urandom | head -c 8)
-
-# Uncomment next line for the first run only
-# echo ""$expname"_$rndstr $PWD $(whoami)" > exptab
+  echo ""$expname"_$rndstr $PWD $(whoami)" > exptab
+else
+  echo "Will use the current exptab: " $(cat exptab)
+fi
 
 export MIDAS_EXPTAB=$(pwd)/exptab
 
 mkdir -p output/root output/mid log/history log/odbdump elog
 
-#git update-index --assume-unchanged exptab
-#end
